cc70a8b48625a0092bbca8a8b7ae8a380b36bdba
@dlt194 <36240712+dlt194@users.noreply.github.com>
2024-10-05 19:39:03 +0100

    Versioning untracked files

---
+++ src/components/CustomCarousel.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import { Card, CardContent } from "@/components/ui/card";
+
+interface CustomCarouselProps {
+  images: string[];
+}
+
+function CustomCarousel({ images }: CustomCarouselProps) {
+  return (
+    <div>
+      <Carousel className="w-full">
+        <CarouselContent className="h-full">
+          {images.map((image, index) => (
+            <CarouselItem key={index} className="h-full">
+              <div className="p-1 h-full">
+                <Card className="h-full">
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <img
+                      src={image}
+                      className="w-full h-full object-scale-down"
+                    />
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+}
+
+export default CustomCarousel;
