4f8b9461c77cd5a7303f84ef3433503c3500c86f
@dlt194 <36240712+dlt194@users.noreply.github.com>
2024-10-05 15:25:19 +0100

    no-unused-vars

--- src/app/webrtc/page.tsx
+++ src/app/webrtc/page.tsx
@@ -9,8 +9,6 @@ import {
 import { Label } from "@/components/ui/label";
 import { Card, CardContent } from "@/components/ui/card";
 
-import { Link2Icon } from "@radix-ui/react-icons";
-
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
